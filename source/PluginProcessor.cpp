08f23af43757e0a90e2e8edf2d14ed9612dd9022
MeijisIrlnd <syl.morrison.dev@gmail.com>
2023-01-22 05:53:29 +0000

    I forgor

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -163,15 +163,17 @@ juce::AudioProcessorEditor* PluginProcessor::createEditor()
 //==============================================================================
 void PluginProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = m_tree.copyState();
+    std::unique_ptr<juce::XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+    if(xmlState.get() == nullptr) return;
+    if(!xmlState->hasTagName(m_tree.state.getType())) return;
+    m_tree.replaceState(juce::ValueTree::fromXml(*xmlState));
 }
 void PluginProcessor::parameterChanged(const juce::String& id, float value) 
 { 
